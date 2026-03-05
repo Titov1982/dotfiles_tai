b25fb57004dc5b60460befd62a5b89dd3575a6cd
Titov Andrey <automatic.spb@mail.ru>
2026-03-05 11:46:02 +0300

    Добавлен запуск tmux с разделением на 3 панели в файл config.fish

--- Linux/.config/fish/config.fish
+++ Linux/.config/fish/config.fish
@@ -1,6 +1,16 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    tmux new-session -A -s main
+    
+    # Создаем новую сессию - main
+    # Создаем новое окно - shell
+    # Делим основное окно горизонтально split-window -h
+    # Делим вторую половину окна вертикально split-window -v
+    # Подключаемся к сессии
+    tmux new -s main -n shell \; split-window -h \; split-window -v \; attach
+
+    # tmux new-session -A -s main
+    # tmux split-window -t main:1 -v '/bin/fish'
+    # tmux split-window -t main:0.0 -h
 end
 
 
