ae100ee3c4de6aad6f3f06fbe544d7627adcfe06
Titov Andrey <automatic.spb@mail.ru>
2026-03-05 09:45:05 +0300

    В файл config.fish добавлена настройка запуска tmux при старте fish

--- Linux/.config/fish/config.fish
+++ Linux/.config/fish/config.fish
@@ -1,7 +1,14 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    tmux new-session -A -s main
 end
 
 
 alias ll="ls -lsAF --color=auto"
 alias bat="batcat"
+
+
+
+# function hello
+#     echo "Hello!"
+# end
